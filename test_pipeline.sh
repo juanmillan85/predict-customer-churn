57188326dce3d6132eea6f5e864dbc78268e996a
juan <1691114+juanmillan85@users.noreply.github.com>
2022-01-04 14:40:08 +0000

    fixing error in the review

--- test_pipeline.sh
+++ test_pipeline.sh
@@ -3,8 +3,8 @@ pylint --output-format=colorized churn_library.py
 pylint --output-format=colorized churn_script_logging_and_tests.py
 autopep8 --in-place --aggressive --aggressive churn_script_logging_and_tests.py
 autopep8 --in-place --aggressive --aggressive churn_library.py
-black churn_library.py
-black churn_script_logging_and_tests.py
+#black churn_library.py
+#black churn_script_logging_and_tests.py
 pytest churn_script_logging_and_tests.py
 
 
